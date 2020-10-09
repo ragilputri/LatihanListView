76086515e39701fdce067bd306872004ab96a97c
ragilputri <3103119151@student.smktelkom-pwt.sch.id>
2020-10-09 13:49:38 -0700

    first commit

---
+++ app/src/main/java/ragilputri/stematel/mylistview/Hero.kt
@@ -0,0 +1,9 @@
+package ragilputri.stematel.mylistview
+
+import android.accounts.AuthenticatorDescription
+
+data class Hero(
+    var photo: Int,
+    var name: String,
+    var description: String
+)
